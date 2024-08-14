022c5743829febb4253db2942234b31f7a521b5f
Vicky Gupta <vicky.k.g@thecodebucket.com>
2024-08-14 13:22:06 +0530

    deactivate category added

--- controllers/quiz/category/index.js
+++ controllers/quiz/category/index.js
@@ -1,53 +1,107 @@
 const category = require("../../../models/quiz/category");
 const { exists } = require("../../../models/quiz/questions");
 
-exports.categoryController = async(req, resp)=>{
-    const {title} = req.body;
-    if(!title){
-        return resp.status(400).json({msg: "Please provide a title for the category"})
-    }
-    try {
-        const findTitle = await category.findOne({title, createdBy: req.admin.id})
-        if(findTitle ) return resp.status(400).json({msg: "Category already exists with this title, please try another title."})
-            
-        const newCategory = new category({title, createdBy: req.admin.id, updatedBy: req.admin.id,})
-        await newCategory.save()
-        return resp.json({msg: "Category created successfully", category: newCategory})
-    } catch (error) {
-        console.log(error)
-        return resp.status(500).json({msg : "Server Error", error})
-    }
-}
+exports.categoryController = async (req, resp) => {
+  const { title } = req.body;
+  if (!title) {
+    return resp
+      .status(400)
+      .json({ msg: "Please provide a title for the category" });
+  }
+  try {
+    const findTitle = await category.findOne({
+      title,
+      createdBy: req.admin.id,
+    });
+    if (findTitle)
+      return resp
+        .status(400)
+        .json({
+          msg: "Category already exists with this title, please try another title.",
+        });
 
-exports.categoryListController = async(req, resp)=>{
-    try {
-        const categories = await category.find({createdBy: req.admin.id}).sort({updatedAt: -1})
-        return resp.json({msg: "All Categories", data : categories})
-    } catch (error) {
-        console.log(error)
-        return resp.status(500).json({msg : "Server Error", error})
-    }
-}
+    const newCategory = new category({
+      title,
+      createdBy: req.admin.id,
+      updatedBy: req.admin.id,
+    });
+    await newCategory.save();
+    return resp.json({
+      msg: "Category created successfully",
+      category: newCategory,
+    });
+  } catch (error) {
+    console.log(error);
+    return resp.status(500).json({ msg: "Server Error", error });
+  }
+};
 
-exports.categoryUpdateController = async(req, resp)=>{
-    const {title} = req.body;
+exports.categoryListController = async (req, resp) => {
+  try {
+    const categories = await category
+      .find({ createdBy: req.admin.id, isActive: true })
+      .sort({ updatedAt: -1 });
+    return resp.json({ msg: "All Categories", data: categories });
+  } catch (error) {
+    console.log(error);
+    return resp.status(500).json({ msg: "Server Error", error });
+  }
+};
+
+exports.categoryUpdateController = async (req, resp) => {
+  const { title } = req.body;
+  const { categoryId } = req.params;
+  if (!title) {
+    return resp
+      .status(400)
+      .json({ msg: "Please provide a title for the category" });
+  }
+  try {
+    const findTitle = await category.findOneAndUpdate(
+      { _id: categoryId, createdBy: req.admin.id },
+      { title, updatedBy: req.admin.id }
+    );
+    if (!findTitle) return resp.status(404).json({ msg: "Category not found" });
+    const updatedCategory = {
+      id: findTitle._id,
+      title: title,
+      createdBy: findTitle.createdBy,
+      updatedBy: req.admin.id,
+      createdAt: findTitle.createdDate,
+    };
+    return resp.json({
+      msg: "Category updated successfully",
+      category: updatedCategory,
+    });
+  } catch (error) {
+    console.log(error);
+    return resp.status(500).json({ msg: "Server Error", error });
+  }
+};
+
+exports.deactivateCategory = async(req, res)=>{
     const {categoryId} = req.params;
-    if(!title){
-        return resp.status(400).json({msg: "Please provide a title for the category"})
-    }
     try {
-        const findTitle = await category.findOneAndUpdate({_id: categoryId, createdBy: req.admin.id}, {title, updatedBy: req.admin.id})
-        if(!findTitle) return resp.status(404).json({msg: "Category not found"})
+        const findCategory = await category.findOneAndUpdate(
+            { _id: categoryId, createdBy: req.admin.id },
+            { isActive: false }
+        );
+        if (!findCategory) return res.status(404).json({ msg: "Category not found" });
         const updatedCategory = {
-    id: findTitle._id,
-    title: title,
-    createdBy: findTitle.createdBy,
-    updatedBy: req.admin.id,
-    createdAt: findTitle.createdDate,}
-        return resp.json({msg: "Category updated successfully", category: updatedCategory})
+            id: findCategory._id,
+            title: findCategory.title,
+            createdBy: findCategory.createdBy,
+            updatedBy: req.admin.id,
+            createdAt: findCategory.createdDate,
+            isActive: false,
+        };
+        return resp.json({
+            msg: "Category deactivated successfully",
+            category: updatedCategory,
+        });
     } catch (error) {
-        console.log(error)
-        return resp.status(500).json({msg : "Server Error", error})
+        console.log(error);
+        return resp.status(500).json({ msg: "Server Error", error });
     }
 }
 
@@ -62,7 +116,7 @@ exports.categoryUpdateController = async(req, resp)=>{
 //         if(!findCategory) return res.status(404).json({msg: "Category not found"})
 //         const questions = await exists.find({categoryId: findCategory._id})
 //         if(questions.length > 0) return res.status(400).json({msg: "Category cannot be deleted as it contains questions})
-            
+
 //             return res.json({msg: "Category deleted successfully"})
 //     } catch (error) {
 //         console.log(error)
--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { adminSignupController, adminLoginController} = require('../../controllers/admin/adminController')
-const { categoryController, categoryListController, categoryUpdateController } = require('../../controllers/quiz/category')
+const { categoryController, categoryListController, categoryUpdateController, deactivateCategory } = require('../../controllers/quiz/category')
 const { adminmiddleware } = require('../../middleware/admin')
 const { typeController } = require('../../controllers/quiz/queTypes')
 const { getTypeController } = require('../../models/quiz/questionTypes/getTypes')
@@ -8,13 +8,22 @@ const { addNewQuestion, getAllAdminquestions, getAdminQuestionById, updateAdminQ
 
 const router = express.Router()
 
+
+// auth
 router.post('/signup', adminSignupController)
 router.post('/login', adminLoginController)
+
+
+// category 
 router.post('/create-category',adminmiddleware, categoryController)
 router.get('/get-category',adminmiddleware, categoryListController)
 router.put('/update-category/:categoryId',adminmiddleware, categoryUpdateController)
+router.post('/delete-category/:categoryId', adminmiddleware, deactivateCategory)
+
+// type
 router.post('/create-type',adminmiddleware, typeController)
 router.get('/get-type',adminmiddleware, getTypeController)
+
 
 // quiz questions routes
 
